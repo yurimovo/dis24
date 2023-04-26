dd10c54ed55e79b873cce8bfef5613e6aef01c5f
yurimovo <yuri22091983@gmail.com>
2023-04-26 16:23:50 +0600

    fix

--- src/service-functions/organizations/createOrganization.ts
+++ src/service-functions/organizations/createOrganization.ts
@@ -2,8 +2,6 @@ import { Organization } from "../../types/organizations";
 
 export const createOrganization = async (data: Organization) => {
   const organizationData = JSON.stringify(data)
-  console.log('data', data);
-  console.log('orgdata', organizationData)
   try {
     const r = await fetch('/api/organizations/organization_add', {
       method: 'POST',
