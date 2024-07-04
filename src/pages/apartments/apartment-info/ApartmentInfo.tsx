21e5aef733a9b6b24605bb3d7f7c181593b7ddd0
yurimovo <yuri22091983@gmail.com>
2024-07-04 16:07:03 +0600

    Apartments and facilities info full

--- src/pages/apartments/apartment-info/ApartmentInfo.tsx
+++ src/pages/apartments/apartment-info/ApartmentInfo.tsx
@@ -50,12 +50,72 @@ const ApartmentInfo = observer(() => {
                         <div className="apartment-info__content__row__content">{ selectedApartment?.address }</div>
                     </div>
                     <div className="apartment-info__content__row">
+                        <div className="apartment-info__content__row__title">Телефоны:</div>
+                        <div className="apartment-info__content__row__content">{ selectedApartment?.phones }</div>
+                    </div>
+                    <div className="apartment-info__content__row">
+                        <div className="apartment-info__content__row__title">ИНН:</div>
+                        <div className="apartment-info__content__row__content">{ selectedApartment?.inn }</div>
+                    </div>
+                    <div className="apartment-info__content__row">
                         <div className="apartment-info__content__row__title">№ договора:</div>
                         <div className="apartment-info__content__row__content">{ selectedApartment?.contruct_number }</div>
                     </div>
                     <div className="apartment-info__content__row">
                         <div className="apartment-info__content__row__title">Дата договора:</div>
                         <div className="apartment-info__content__row__content">{ selectedApartment?.contruct_date }</div>
+                    </div>
+                    <div className="apartment-info__content__row">
+                        <div className="apartment-info__content__row__title">Тариф в месяц:</div>
+                        <div className="apartment-info__content__row__content">{ selectedApartment?.price }</div>
+                    </div>
+                    <div className="apartment-info__content__row">
+                        <div className="apartment-info__content__row__title">Дата тарифа:</div>
+                        <div className="apartment-info__content__row__content">{ selectedApartment?.contruct_date }</div>
+                    </div>
+                    <div className="apartment-info__content__row">
+                        <div className="apartment-info__content__row__title">Вид охраны:</div>
+                        <div className="apartment-info__content__row__content">{ selectedApartment?.security_type }</div>
+                    </div>
+                    <div className="apartment-info__content__row">
+                        <div className="apartment-info__content__row__title">№ договорного дела:</div>
+                        <div className="apartment-info__content__row__content">{ selectedApartment?.contruct_file_number }</div>
+                    </div>
+                    <div className="apartment-info__content__row">
+                        <div className="apartment-info__content__row__title">№ литерного дела:</div>
+                        <div className="apartment-info__content__row__content">{ selectedApartment?.lettered_file_number }</div>
+                    </div>
+                    <div className="apartment-info__content__row">
+                        <div className="apartment-info__content__row__title">Категория МПХИГ:</div>
+                        <div className="apartment-info__content__row__content">{ selectedApartment?.apartment_category }</div>
+                    </div>
+                    <div className="apartment-info__content__row">
+                        <div className="apartment-info__content__row__title">№ пенала:</div>
+                        <div className="apartment-info__content__row__content">{ selectedApartment?.penal_number }</div>
+                    </div>
+                    <div className="apartment-info__content__row">
+                        <div className="apartment-info__content__row__title">Пультовый №:</div>
+                        <div className="apartment-info__content__row__content">{ selectedApartment?.pult_number }</div>
+                    </div>
+                    <div className="apartment-info__content__row">
+                        <div className="apartment-info__content__row__title">СПИ:</div>
+                        <div className="apartment-info__content__row__content">{ selectedApartment?.spi }</div>
+                    </div>
+                    <div className="apartment-info__content__row">
+                        <div className="apartment-info__content__row__title">Оборудование:</div>
+                        <div className="apartment-info__content__row__content">{ selectedApartment?.apartment_hardware }</div>
+                    </div>
+                    <div className="apartment-info__content__row">
+                        <div className="apartment-info__content__row__title">Монтажная организация:</div>
+                        <div className="apartment-info__content__row__content">{ selectedApartment?.mounting_organization }</div>
+                    </div>
+                    <div className="apartment-info__content__row">
+                        <div className="apartment-info__content__row__title">Обслуживающая организация:</div>
+                        <div className="apartment-info__content__row__content">{ selectedApartment?.surving_organization }</div>
+                    </div>
+                    <div className="apartment-info__content__row">
+                        <div className="apartment-info__content__row__title">Подбор:</div>
+                        <div className="apartment-info__content__row__content">{ selectedApartment?.assortment }</div>
                     </div>
                 </div>
             }
--- src/pages/facilities/facility-info/FacilityInfo.tsx
+++ src/pages/facilities/facility-info/FacilityInfo.tsx
@@ -60,6 +60,70 @@ const FacilityInfo = observer(() => {
                         <div className="facility-info__content__row__title">Дата договора:</div>
                         <div className="facility-info__content__row__content">{ selectedFacility?.contruct_date }</div>
                     </div>
+                    <div className="facility-info__content__row">
+                        <div className="facility-info__content__row__title">Тариф в месяц:</div>
+                        <div className="facility-info__content__row__content">{ selectedFacility?.price }</div>
+                    </div>
+                    <div className="facility-info__content__row">
+                        <div className="facility-info__content__row__title">Дата тарифа:</div>
+                        <div className="facility-info__content__row__content">{ selectedFacility?.price_date }</div>
+                    </div>
+                    <div className="facility-info__content__row">
+                        <div className="facility-info__content__row__title">Форма собственности:</div>
+                        <div className="facility-info__content__row__content">{ selectedFacility?.ownership_type }</div>
+                    </div>
+                    <div className="facility-info__content__row">
+                        <div className="facility-info__content__row__title">Категория объекта:</div>
+                        <div className="facility-info__content__row__content">{ selectedFacility?.facility_category }</div>
+                    </div>
+                    <div className="facility-info__content__row">
+                        <div className="facility-info__content__row__title">Вид охраны:</div>
+                        <div className="facility-info__content__row__content">{ selectedFacility?.security_type }</div>
+                    </div>
+                    <div className="facility-info__content__row">
+                        <div className="facility-info__content__row__title">№ договорного дела:</div>
+                        <div className="facility-info__content__row__content">{ selectedFacility?.contruct_file_number }</div>
+                    </div>
+                    <div className="facility-info__content__row">
+                        <div className="facility-info__content__row__title">№ литерного дела:</div>
+                        <div className="facility-info__content__row__content">{ selectedFacility?.lettered_file_number }</div>
+                    </div>
+                    <div className="facility-info__content__row">
+                        <div className="facility-info__content__row__title">СПИ:</div>
+                        <div className="facility-info__content__row__content">{ selectedFacility?.spi }</div>
+                    </div>
+                    <div className="facility-info__content__row">
+                        <div className="facility-info__content__row__title">Оборудование:</div>
+                        <div className="facility-info__content__row__content">{ selectedFacility?.facility_hardware }</div>
+                    </div>
+                    <div className="facility-info__content__row">
+                        <div className="facility-info__content__row__title">Монтажная организация:</div>
+                        <div className="facility-info__content__row__content">{ selectedFacility?.mounting_organization }</div>
+                    </div>
+                    <div className="facility-info__content__row">
+                        <div className="facility-info__content__row__title">Обслуживающая организация:</div>
+                        <div className="facility-info__content__row__content">{ selectedFacility?.surving_organization }</div>
+                    </div>
+                    <div className="facility-info__content__row">
+                        <div className="facility-info__content__row__title">Пультовый №:</div>
+                        <div className="facility-info__content__row__content">{ selectedFacility?.pult_number }</div>
+                    </div>
+                    <div className="facility-info__content__row">
+                        <div className="facility-info__content__row__title">№ СИМ-карты:</div>
+                        <div className="facility-info__content__row__content">{ selectedFacility?.sim_number }</div>
+                    </div>
+                    <div className="facility-info__content__row">
+                        <div className="facility-info__content__row__title">Ответственный:</div>
+                        <div className="facility-info__content__row__content">{ selectedFacility?.responsible }</div>
+                    </div>
+                    <div className="facility-info__content__row">
+                        <div className="facility-info__content__row__title">Подбор:</div>
+                        <div className="facility-info__content__row__content">{ selectedFacility?.assortment }</div>
+                    </div>
+                    <div className="facility-info__content__row">
+                        <div className="facility-info__content__row__title">Часы охраны:</div>
+                        <div className="facility-info__content__row__content">{ selectedFacility?.security_hours }</div>
+                    </div>
                 </div>
             }
         </div>
